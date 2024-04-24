450e1c4969c5333eb16863923a57c7fab8ea4d75
Sanhil143 <sanhilrai143@gmail.com>
2024-04-25 02:05:42 +0530

    navbar ui has done

--- src/components/common/Navbar.jsx
+++ src/components/common/Navbar.jsx
@@ -1,11 +1,65 @@
-import React from 'react'
+import React from "react";
+import "../../css/Navbar.css";
 
 const Navbar = () => {
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <div
+      style={{
+        width: "100%",
+        height: "60px",
+        display: "flex",
+      }}
+    >
+      <div
+        style={{
+          display: "flex",
+          width: "100%",
+          height: "100%",
+          // border: "1px solid black",
+          boxShadow:"1px 1px 10px 2px grey",
+          alignItems: "center",
+          justifyContent: "space-between",
+        }}
+      >
+        <div
+          style={{
+            display: "flex",
+            alignItems: "center",
+          }}
+        >
+          <h2
+            style={{
+              marginLeft: "20px",
+            }}
+          >
+            School Management
+          </h2>
+          <ul
+            style={{
+              display: "flex",
+              listStyle: "none",
+              marginLeft: "160px",
+            }}
+          >
+            <li className="navLi">Students</li>
+            <li className="navLi">Teachers</li>
+            <li className="navLi">Classes</li>
+          </ul>
+        </div>
 
-export default Navbar
+        <ul
+          style={{
+            display: "flex",
+            listStyle: "none",
+            marginRight: "40px",
+          }}
+        >
+          <li className="navLi">Profile</li>
+          <li className="navLi">Logout</li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
