e8a051767174455f82fc7fdb85ed49cc4096edd9
Sanhil143 <sanhilrai143@gmail.com>
2024-04-26 01:33:43 +0530

    school page has done with api

--- src/components/pages/School.jsx
+++ src/components/pages/School.jsx
@@ -1,6 +1,29 @@
-import React from 'react'
+import axios from 'axios'
+import React, { useEffect, useState } from 'react'
+import {dateFormatter} from '../../utils/dateFormater'
 
 const School = () => {
+  const [school,setSchool] = useState([])
+  const userId = localStorage.getItem('userId')
+  const token = localStorage.getItem('accessToken')
+  const fetchSchool = async () => {
+    try {
+      const response = await axios.get(`http://localhost:3000/api/v1/school/getMySchool/${userId}`,{
+        headers:{
+          Authorization: `bearer ${token}`
+        }
+      });
+      if(response.data){
+        console.log(response.data);
+        setSchool(response.data[0])
+      }
+    } catch (error) {
+      console.log(error.message)
+    }
+  }
+  useEffect(() => {
+    fetchSchool()
+  },[])
   return (
     <div style={{
       height:"100vh",
@@ -14,12 +37,17 @@ const School = () => {
         marginTop:"80px",
         textAlign:"center"
       }}>
-      <h1>school</h1>
+      <h1>{school.name}</h1>
       <img style={{
         marginTop:"10px",
         height:"400px",
         width:"600px"
       }} src='/school-image.avif' alt='schoolImage'/>
+      <p style={{
+        marginTop:"40px",
+        fontWeight:"bold",
+        textAlign:"left"
+        }}>Established at : {dateFormatter(school.createdAt)}</p>
       </div>
     </div>
   )
--- src/components/pages/Students.jsx
+++ src/components/pages/Students.jsx
@@ -2,27 +2,9 @@ import axios from 'axios';
 import React, { useEffect, useState } from 'react'
 
 const Students = () => {
-  const [students,setStudents] = useState([]);
   const userId = localStorage.getItem('userId')
   const token = localStorage.getItem('accessToken')
-  const fetchData = async () => {
-    try {
-      const response = await axios.get(`http://localhost:3000/api/v1/school/getMySchool/${userId}`,{
-        headers:{
-          Authorization: `bearer ${token}`
-        }
-      });
-      if(response.data){
-        console.log(response.data);
-      }
-    } catch (error) {
-      console.error(error.message)
-    }
-  }
-
-  useEffect(() => {
-    fetchData()
-  },[]);
+  
   return (
     <div>
       
