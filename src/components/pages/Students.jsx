efd6027aa1c88b248e3441f660b3fa93336836f5
Sanhil143 <sanhilrai143@gmail.com>
2024-04-26 01:01:54 +0530

    update navbar sccol setup & add shool page

--- src/components/pages/Students.jsx
+++ src/components/pages/Students.jsx
@@ -1,9 +1,31 @@
-import React from 'react'
+import axios from 'axios';
+import React, { useEffect, useState } from 'react'
 
 const Students = () => {
+  const [students,setStudents] = useState([]);
+  const userId = localStorage.getItem('userId')
+  const token = localStorage.getItem('accessToken')
+  const fetchData = async () => {
+    try {
+      const response = await axios.get(`http://localhost:3000/api/v1/school/getMySchool/${userId}`,{
+        headers:{
+          Authorization: `bearer ${token}`
+        }
+      });
+      if(response.data){
+        console.log(response.data);
+      }
+    } catch (error) {
+      console.error(error.message)
+    }
+  }
+
+  useEffect(() => {
+    fetchData()
+  },[]);
   return (
     <div>
-      <h1>students</h1>
+      
     </div>
   )
 }
