6a917dfde1690a1d1d16440b5578642997667ffb
Sanhil143 <sanhilrai143@gmail.com>
2024-04-28 02:00:29 +0530

    student page has done with student detail page

--- src/components/pages/Students.jsx
+++ src/components/pages/Students.jsx
@@ -1,8 +1,10 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
+import StudentDetail from "./StudentDetail";
 
 const Students = ({ schoolId }) => {
   const [student, setStudent] = useState([]);
+  const [selectStudent, setSelectStudent] = useState(null);
   const token = localStorage.getItem("accessToken");
 
   const fetchStudent = async () => {
@@ -28,42 +30,54 @@ const Students = ({ schoolId }) => {
     fetchStudent();
   }, []);
 
+  const handleSelectStudent = (selectStudent) => {
+    setSelectStudent(selectStudent);
+  };
+
+  const handleCancelButton = () => {
+    setSelectStudent(null);
+  };
+
   return (
-    <div
-      style={{
-        display: "flex",
-        flexWrap: "wrap",
-        width: "50%",
-        height: "100vh",
-        margin: "auto",
-      }}
-    >
-      {student.map((stu) => (
-        <div
-          key={stu.studentId}
-          style={{
-            width: "150px",
-            height: "150px",
-            marginTop: "35px",
-            marginLeft: "20px",
-            textAlign:"center",
-            cursor:"pointer"
-          }}
-        >
-          <img
+    <div>
+      <div
+        style={{
+          display: "flex",
+          flexWrap: "wrap",
+          width: "50%",
+          height: "100vh",
+          margin: "auto",
+        }}
+      >
+        {student.map((stu) => (
+          <div
+            key={stu.studentId}
+            onClick={() => handleSelectStudent(stu)}
             style={{
-              width: "80%",
-              height: "100%",
-              borderRadius:"10px",
+              width: "150px",
+              height: "150px",
+              marginTop: "35px",
+              marginLeft: "20px",
+              textAlign: "center",
+              cursor: "pointer",
             }}
-            src="/san.jpg"
-            alt="studentImage"
-          />
-          <h5 style={{marginTop:"5px"}}>
-            {stu.firstName} {stu.lastName}
-          </h5>
-        </div>
-      ))}
+          >
+            <img
+              style={{
+                width: "80%",
+                height: "100%",
+                borderRadius: "10px",
+              }}
+              src="/san.jpg"
+              alt="studentImage"
+            />
+            <h5 style={{ marginTop: "5px" }}>
+              {stu.firstName} {stu.lastName}
+            </h5>
+          </div>
+        ))}
+      </div>
+      <StudentDetail students={selectStudent} cancel={handleCancelButton} />
     </div>
   );
 };
